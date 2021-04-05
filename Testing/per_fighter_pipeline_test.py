92b2a690ca4dbb9f290e063d87bb37a7de59b175
John Stokes <johnclstokes@hotmail.com>
2021-04-05 12:51:55 +0100

    split master df into a per fighter df, added a so far empty file to be used for unit testing the pipeline.

---
+++ Testing/per_fighter_pipeline_test.py
@@ -0,0 +1,4 @@
+"""
+Unit tests for per_fighter_pipeline.py. These are fairly simple functions so this is more of a formality tbh.
+
+"""
--- per_fighter_pipeline.py
+++ per_fighter_pipeline.py
@@ -1,10 +1,10 @@
 """
 The data as downloaded is on a mostly per fight basis, we'd like to convert it into a per fighter form of some sort.
-some wrangling will probs be required.
+Some wrangling will probs be required. Maybe a cheeky bit of slap and tickle as well, who knows...
 
-TODO: - identify what columns should be salvaged and how
-      - write pipeline for preforming the transformation
-      - build tests where neccessary
+TODO: - identify what columns should be salvaged and how  <- more likely I'll be adding than taking away tbh
+      - write pipeline for preforming the transformation  <- 1/2 done
+      - build tests where necessary  <- so far nothing test worthy :(
       - set up sphinx docs <- looks like we need something to allow for numpy docs
 """
 import pandas as pd
@@ -33,10 +33,10 @@ def set_mixed_dtype_to_int(
 
 
 def create_col_hash(
-        df: pd.DataFrame,
-        cols_to_hash: Tuple[pd.Series],
+    df: pd.DataFrame,
+    cols_to_hash: Tuple[pd.Series, ...],
 ) -> pd.DataFrame:
-    """Create 16 digit hash function using specified columns.
+    """Create 16 character alphanumeric hash function using specified columns.
 
     Parameters
     ----------
@@ -49,7 +49,7 @@ def create_col_hash(
     """
     df['bout_id'] = (
         cols_to_hash
-        .apply(lambda x: hashlib.md5(x.encode('utf-8')).hexdigest())  # alphanumeric hash per fight
+        .apply(lambda x: hashlib.md5(x.encode('utf-8')).hexdigest())
     )
 
     return df
@@ -81,5 +81,65 @@ def get_master_df() -> pd.DataFrame:
     return df
 
 
+def split_df_by_col(
+    df: pd.DataFrame,
+    exc_substring: str = 'R_',
+    inc_substring: str = 'B_',
+    corner: str = 'Blue',
+) -> pd.DataFrame:
+    """Create per fighter df by taking only columns relevant to the bout on the whole and the specified corner.
+
+    Parameters
+    ----------
+    df : Input dataframe pertaining to both fighters.
+    exc_substring : Substring of column names to exclude from output df.
+    inc_substring : Substring of column names that will be included, but without this substring.
+    corner : Corner the specified fighter was fighting out of.
+
+    Notes
+    -----
+    We need to maintain the knowledge of which corner the fighter came from as some columns such as 'winner' are given
+    to the corner not the fighter. For now, this will be done by adding a 'corner' column.
+    TODO: convert these columns to binary where appropriate? or at least make them not corner dependant...
+
+    Returns
+    -------
+    Only data pertaining to the specified corner / the whole bout, with generic column names.
+    """
+    opponent = '{}fighter'.format(exc_substring)
+
+    columns_to_keep = [col for col in df.columns if exc_substring not in col]
+    columns_to_keep.append(opponent)
+
+    column_names = {col: col.replace(inc_substring, '') for col in columns_to_keep}
+    column_names[opponent] = 'opponent'
+
+    per_fighter_df = df[columns_to_keep].copy()
+    per_fighter_df.rename(
+        columns=column_names,
+        inplace=True
+    )
+    per_fighter_df['corner'] = corner
+
+    return per_fighter_df
+
+
+def create_per_fighter_df() -> pd.DataFrame:
+    """Read in and split the master data into a per fighter dataframe.
+
+    Returns
+    -------
+    Per fighter form of the master data.
+    """
+    df = get_master_df()
+
+    red_corner_df = split_df_by_col(df, exc_substring='B_', inc_substring='R_', corner='Red')
+    blue_corner_df = split_df_by_col(df, exc_substring='R_', inc_substring='B_', corner='Blue')
+
+    df = pd.concat([red_corner_df, blue_corner_df])
+
+    return df
+
+
 if __name__ == '__main__':
-    pass
+    pass  # This will eventually run the bloody bastard pipeline.
